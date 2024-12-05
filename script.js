6e3d0221599d6c5ef09727bb2085811ee7c27b5a
Sebastien-cloud <bastien0290@hotmail.fr>
2024-12-05 17:11:00 +0100

    modiifs

--- script.js
+++ script.js
@@ -15,10 +15,10 @@ function afficherLesInstruments(i) {
     // role afficher la carte d'un instrument dnas la page html dans la div qui a l'id Instruments
     // pas de retour
 
-    let template = `<div id="Instruments">
+    let template = `<div class="cardInstru">
                 <a href="">
                     <img src="${i.imageUrl}" class="tailleImg" alt="instrument de musique hang"></a>
-                <p>${i.nom}</p>
+                <h5>${i.nom}:</h5>
                 <p class="widthP">${i.description}</p>
             </div>`
 
@@ -39,11 +39,11 @@ fetch("data.json.json")
     //parametre: un prenom
     // role: afficher le prenom d'une personne qui a temoigne dans la page html qui a l'id Temoignages
     //retour: aucun
-    let template = `<div id="Temoignages">
-                <p>${t.prenom}:</p>
-                <p>${t.prestation}:</p>
-                <p>${t.commentaire}:</p>
-                <p>${t.note}: /5</p>
+    let template = `<div class="cardTemoignages">
+                <h5>${t.prenom}</h5>
+                <p>${t.prestation}</p>
+                <p>${t.commentaire}</p>
+                <h5>${t.note} /5</h5>
             </div>`
 
     document.querySelector("#Temoignages").innerHTML += template
