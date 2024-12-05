c7730292e3b689625c57a525103a1ec9fe7d7119
Sebastien-cloud <bastien0290@hotmail.fr>
2024-12-05 14:33:16 +0100

    modif

--- script.js
+++ script.js
@@ -4,28 +4,4 @@ fetch("data.json.json")
 })
 .then(donnee=>{
     console.log(donnee)
-    afficheInfos(donnee)
-    // j'affiche les produits
-    // donnee.produits est un tableau
-    //je bouche sur le tableau et pour chacun des pproduit : je l'affiche
-    donnee.produits.forEach(produit => {
-        afficheUnProduit(produit)
-    });
 })
-function afficheInfos(info){
-    document.querySelector("#produit").innerHTML = info.produits
-}
-function afficheUnProduit(p){
-    // ROle: affiche un produit dans la div qui a l'id "produits"
-    //parametre : p, le produit a afficher
-    // retour rien
-    console.log(p.nom)
-    console.log(p.description)
-    console.log(p.imageUrl)
-    let template = `<div class="produit">
-            <img src="${p.imageUrl}" alt="">
-            <h2>${p.nom}</h2>
-            <p>${p.description}</p>
-        </div>`
-    document.querySelector("#produit").innerHTML += template
-}
